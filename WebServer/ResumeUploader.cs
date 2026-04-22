9d9b0c9c606680df1cf97dcec69424298d6806e1
Juice Box <234071437+JuiceBox7280@users.noreply.github.com>
2026-04-21 21:26:10 -0400

    Added frontend resume upload functionality (txt, doc(x), pdf).

--- WebServer/ResumeUploader.cs
+++ WebServer/ResumeUploader.cs
@@ -1,7 +1,13 @@
 namespace JobRush;
 /// <summary>
-/// Handles resume upload process. This class is unique for each client connection.
+/// Handles resume uploads. This class is unique for each client connection.
 /// </summary>
 internal class ResumeUploader {
-	
+	public bool Upload(MemoryStream resumeBytes) {
+		resumeBytes.Position = 0; // Ensures referenced stream is read from the beginning.
+		// TODO: Attempt to store resume in DB, return false on fail.
+		// TODO: Notify preprocessor of new resume.
+		// TODO: Add documentation.
+		throw new NotImplementedException();
+	}
 }
